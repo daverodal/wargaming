4f5d73bdc3b64da28f105f46f758f716114d889b
david <dave.rodal@gmail.com>
2016-06-05 11:01:29 -0400

    NTA rule type

--- Wargame/NTA/victoryConditions.blade.php
+++ Wargame/NTA/victoryConditions.blade.php
@@ -1,4 +1,4 @@
 <h2>Victory Conditions</h2>
-<h4>The game ends at the end of turn 7. The player last occupied or passed through the "tree of victory" (Arbre de la Victoire) wins the game.
+<h4>The game ends at the end of turn 7. The player that last occupied or passed through the "tree of victory" (Arbre de la Victoire) wins the game.
     If nobody passed through it, the game is a tie</h4>
 
