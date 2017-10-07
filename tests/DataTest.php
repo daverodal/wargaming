966163510278e9f08e49de1db7efa817412541c6
david <dave.rodal@gmail.com>
2017-10-07 13:30:55 -0400

    unit test fix?

--- tests/DataTest.php
+++ tests/DataTest.php
@@ -18,7 +18,7 @@
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-class DataTest extends PHPUnit_Framework_TestCase
+class DataTest extends PHPUnit\Framework\TestCase
 {
     /**
      * @dataProvider provider
--- tests/ScenarioTest.php
+++ tests/ScenarioTest.php
@@ -1,5 +1,5 @@
 <?php
-class ScenarioTest extends PHPUnit_Framework_TestCase
+class ScenarioTest extends PHPUnit\Framework\TestCase
 {
 
 
