0395d248973adee80b47cce7f518167174c64649
david <dave.rodal@gmail.com>
2017-10-21 13:30:11 -0400

    webpack.mix.js fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,19 +23,19 @@ mix.webpackConfig({
     ]});
 mix.setPublicPath("../game-dispatcher/public/vendor/");
 mix.sass('Wargame/Mollwitz/all-nations-colors.scss', 'css/wargame/all-nations-colors.css')
-mix.sass('Wargame/TMCW/RetreatOne/all.scss', 'css/wargame/retreatOne.css')
-mix.sass('Wargame/TMCW/Airborne/all.scss', 'css/wargame/amph.css');
+// mix.sass('Wargame/TMCW/RetreatOne/all.scss', 'css/wargame/retreatOne.css')
+// mix.sass('Wargame/TMCW/Airborne/all.scss', 'css/wargame/amph.css');
 // mix.sass('Wargame/TMCW/Amph/all.scss', 'css/wargame/airborne.css');
-mix.sass('Wargame/TMCW/KievCorps/all.scss', 'css/wargame/kievCorps.css');
-mix.sass('Wargame/TMCW/Manchuria1976/all.scss', 'css/wargame/manchuria1976.css');
-mix.sass('Wargame/TMCW/Chawinda1965/all.scss', 'css/wargame/chawinda1965.css');
+// mix.sass('Wargame/TMCW/KievCorps/all.scss', 'css/wargame/kievCorps.css');
+// mix.sass('Wargame/TMCW/Manchuria1976/all.scss', 'css/wargame/manchuria1976.css');
+// mix.sass('Wargame/TMCW/Chawinda1965/all.scss', 'css/wargame/chawinda1965.css');
 
 mix.js('wargame.js', 'javascripts/wargame/wargame.js');
-mix.js('Wargame/TMCW/RetreatOne/retreatOne.js', 'javascripts/wargame/retreatOne.js');
-mix.js('Wargame/TMCW/KievCorps/kievCorps.js', 'javascripts/wargame/kievCoprs.js');
-mix.js('Wargame/TMCW/Amph/amph.js', 'javascripts/wargame/amph.js');
-mix.js('Wargame/TMCW/Airborne/airborne.js', 'javascripts/wargame/airborne.js');
-mix.js('Wargame/TMCW/Moskow/moskow.js', 'javascripts/wargame/moskow.js');
+// mix.js('Wargame/TMCW/RetreatOne/retreatOne.js', 'javascripts/wargame/retreatOne.js');
+// mix.js('Wargame/TMCW/KievCorps/kievCorps.js', 'javascripts/wargame/kievCoprs.js');
+// mix.js('Wargame/TMCW/Amph/amph.js', 'javascripts/wargame/amph.js');
+// mix.js('Wargame/TMCW/Airborne/airborne.js', 'javascripts/wargame/airborne.js');
+// mix.js('Wargame/TMCW/Moskow/moskow.js', 'javascripts/wargame/moskow.js');
 
 
 mix.setResourceRoot("/vendor/").sass('Wargame/Mollwitz/horse-musket.scss', 'css/wargame/horse-musket.css')
