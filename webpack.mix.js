52de6c4bec2d47cb8a3f0fca5ee8d7feecd1efba
david_rodal <dave.rodal@gmail.com>
2018-01-03 13:14:05 -0500

    removal of old  stuff

--- webpack.mix.js
+++ webpack.mix.js
@@ -45,12 +45,12 @@ mix.setPublicPath("../game-dispatcher/public/vendor/");
 mix.copyDirectory('Wargame/wargame-helpers/audio','../game-dispatcher/public/assets/audio')
     .copyDirectory('Wargame/wargame-helpers/unit-images', '../game-dispatcher/public/assets/unit-images');
 mix.sass('Wargame/Mollwitz/all-nations-colors.scss', 'css/wargame/all-nations-colors.css')
-// mix.sass('Wargame/TMCW/RetreatOne/all.scss', 'css/wargame/retreatOne.css')
+mix.sass('Wargame/TMCW/RetreatOne/all.scss', 'css/wargame/retreatOne.css')
 mix.sass('Wargame/TMCW/Airborne/all.scss', 'css/wargame/airborne.css');
 mix.sass('Wargame/TMCW/Amph/all.scss', 'css/wargame/amph.css');
 mix.sass('Wargame/TMCW/MartianCivilWar/all.scss', 'css/wargame/martiancivilwar.css');
-// mix.sass('Wargame/TMCW/KievCorps/all.scss', 'css/wargame/kievCorps.css');
-// mix.sass('Wargame/TMCW/Manchuria1976/all.scss', 'css/wargame/manchuria1976.css');
+mix.sass('Wargame/TMCW/KievCorps/all.scss', 'css/wargame/kievCorps.css');
+mix.sass('Wargame/TMCW/Manchuria1976/all.scss', 'css/wargame/manchuria1976.css');
 mix.sass('Wargame/TMCW/Chawinda1965/all.scss', 'css/wargame/chawinda1965.css');
 mix.sass('Wargame/NTA/all.scss', 'css/wargame/nta.css');
 
