4eecb814ac680be4f11a4269a7b20e4981bf59d7
david <dave.rodal@gmail.com>
2019-06-08 13:04:39 -0400

    freemans farm rules fix

--- Wargame/Mollwitz/FreemansFarm1777/exclusiveRules.blade.php
+++ Wargame/Mollwitz/FreemansFarm1777/exclusiveRules.blade.php
@@ -27,46 +27,63 @@ You should have received a copy of the GNU General Public License
 ?>
 
     <li><span class="lessBig">Deploy Phase</span>
-    <li><?= $deployName[1] ?>: player deploys first.</li>
-    <li><?= $deployName[2] ?>: player deploys second.
-    </li>
-    </li>
-    <li><span class="lessBig">Movement</span>
         <ol>
-            <li><?= $forceName[1] ?>: player moves first.</li>
-            <li><?= $forceName[2] ?>: player moves second.
+            <li>
+                <?= $deployName[2] ?> deploy first move first. Deploy on L and LD hexes
+                Units deployed on LD hexes will not be able to move until turn 4.
+                    Units deployed on the L hexes will not be able to move on turn 1.
+                Stackable units may (and in fact must) deploy stacked.
             </li>
-            <li> On turn one the <?= $forceName[1] ?> may not move.</li>
-            <li>The Hessian units deployed along the Hudson river may not move till turn 4.</li>
+            <li>
+                <?= $deployName[1] ?> Deploy second move second deploy on L hexes.
+            </li>
+        </ol>
+    </li>
+    <li><span class="lessBig">Exclusion Zones</span>
+        <ol>
+            <li>
+                Only Loyalist units may enter Red E hexes
+
+            </li>
+            <li>
+                Only Rebel units may enter Blue E hexes
+            </li>
         </ol>
     </li>
     <li>
-        <span class="lessBig">Special Units</span>
+        <span class="lessBig">Stackable Units</span>
         <ol>
-            <li><span class="lessBig">Morgan</span>
-                The <?= $forceName[2] ?> player has a special unit, Morgan. This unit represents the sharp
-                shooter associated with Morgans troops. To this end, they have a range of two, and can
-                hit units 2 hexes away. The only restriction is they may NOT shoot over units friendly or enemy, Nor
-                may shoot along the hexside between two units. They MAY shoot along the hexside of just one unit.
-            </li>
-            <li> <span class="lessBig">Arnold</span>
-                The HQ units represents Arnold, arnold may stack with, attack with and defend with Another unit.
-                adding 3 to any attack.
-            </li>
             <li>
-                <span class="lessBig">
-                    Combat Bonus
-                </span>
-                If Morgan or Arnold participate in an attack there is a +1 shift for the attacker.
-                These cannot be combined for greater than +1 shift, but you may have two seperate attacks with
-                a +1 shift.
-            </li>
-            <li>
-                <span class="lessBig">Small units</span>
-                Several of the <?= $forceName[1] ?> are considered Small units and have an S on them between the two numbers
-                These units MAY stack with other units, they DO NOT contribute to a combat shift for artillery UNLESS there
-                are 3 attack strength point of artillery.
+                Stackable unit are marked by an S between Combat and move factors. Add their factors too units stacked with on defense and Attack;
+                except arty not doubled on defense when stacked. Stacked units all share the same fate in combat results.
+                Two stackable units may stack with each other but there may never be more than a total of two units in a hex.
             </li>
         </ol>
     </li>
+<li>
+    <span class="lessBig">Charismatic leader Benedict Arnold</span>
+    <ol>
+        <li>
+            Arnold may stack with another unit even if it has another stackable unit with it,
+            adds Combat factor to stack. Has no combat factor when not stacked.
+        </li>
+        <li>
+            When attacking He shifts a column one to right.
+        </li>
+        <li>
+            He is destroyed if the unit he is stacked with is destroyed. He is destroyed if a hostile moves into his hex and he is not stacked with a friendly.
+        </li>
+    </ol>
+</li>
+<li>
+    <span class="lessBig">Morgan’s Rifles (Rebel 6-4 with range of 2)</span>
+    <ol>
+        <li>Has a range of 2 may not fire over other units, Nor may it shoot along the hexside between two units. It MAY shoot along the hexside of just one unit.
+            It follows other artillery line of site rules.Morgan like Artillery is not halved attacking across streams, rivers.
+        </li>
+        <li>
+            It provides a column shift to the right when attacking.
+        </li>
+    </ol>
+</li>
 
