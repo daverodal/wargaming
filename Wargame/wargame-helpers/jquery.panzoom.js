5a5555eb9ce3e0a8e9f3c20d00ec0480ccb40d4d
david_rodal <david.rodal@mheducation.com>
2019-07-28 12:09:37 -0400

    fixed jqueryPanzoom bleh

--- Wargame/wargame-helpers/jquery.panzoom.js
+++ Wargame/wargame-helpers/jquery.panzoom.js
@@ -245,7 +245,11 @@
 
         // Build the appropriately-prefixed transform style property name
         // De-camelcase
-        this._transform = $.cssProps.transform.replace(rupper, '-$1').toLowerCase();
+        if(!$.cssProps.transform){
+            this._transform = {};
+        }else{
+            this._transform = $.cssProps.transform.replace(rupper, '-$1').toLowerCase();
+        }
 
         // Build the transition value
         this._buildTransition();
