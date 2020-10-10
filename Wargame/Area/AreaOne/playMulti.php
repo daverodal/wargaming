4ecd51f5660035a28fb928206711f196c99bd9b8
David Rodal <dave.rodal@gmail.com>
2020-10-10 16:05:35 -0400

    fix for back ground error message

--- Wargame/Area/AreaOne/playMulti.php
+++ Wargame/Area/AreaOne/playMulti.php
@@ -19,9 +19,10 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-?><style type="text/css">
-    <?php
-    @include "playMulti.css";?>
+?>
+
+<style type="text/css">
+
     body {
         background: #ccc;
         color: #333;
