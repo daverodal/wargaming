61f09190d6194bc826400bfec938076815f88523
David Rodal <daverodal@users.noreply.github.com>
2021-12-26 16:34:02 -0500

    removed console.logs

--- Wargame/wargame-helpers/Vue/VueCrt.vue
+++ Wargame/wargame-helpers/Vue/VueCrt.vue
@@ -96,7 +96,6 @@
             },
             pinCombat(index, e){
               return;
-              console.log(e);
                 const x = DR.dragged;
                 if(DR.dragged){
                     return;
