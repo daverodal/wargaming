755b3bac122b21c67806027732e4b742d5bee211
David Rodal <daverodal@users.noreply.github.com>
2023-05-29 09:04:25 -0400

    poor wip stab at fixing eastwestforce and combining and splitting units. wip wip wip

--- Wargame/EastWestForce.php
+++ Wargame/EastWestForce.php
@@ -21,8 +21,57 @@ class EastWestForce extends Force {
         return;
     }
 
+    public function findSimilarGerman($unit){
+        $b = Battle::getBattle();
+        /* @var mapData $mapData */
+        $mapData = $b->mapData;
+        $units = $mapData->getHex($unit->hexagon->name)->getForces($unit->forceId);
+        $similarUnits = [];
+        $infStack = true;
+        $armorStack = true;
+        $hasAlly = false;
+        if($unit->class === 'inf'){
+            $armorStack = false;
+            if($unit->maxMove === 2) {
+                $hasAlly = true;
+            }
+        }
+        if($unit->class === 'armor'){
+            $infStack = false;
+        }
+        $unitCount = 0;
+        foreach($units as $k => $v){
+            $otherUnit = $this->units[$k];
+            if($otherUnit->forceId === $unit->forceId){
+                if( $k != $unit->id){
+                    $unitCount++;
+                    if($otherUnit->class === 'inf'){
+                        $armorStack = false;
+                        if($unit->maxMove === 2) {
+                            $hasAlly = true;
+                        }
+                    }
+                    if($otherUnit->class === 'armor'){
+                        $infStack = false;
+                    }
+                }
+
+            }
+        }
+        if($infStack && $unitCount === 2){
+            $unit->combine($units, 'inf');
+        }
+        if($armorStack && $unitCount === 2){
+            $unit->combine($units, 'armor');
+        }
+        return ($infStack || $armorStack);
+    }
+
     public function findSimilarInHex($unit)
     {
+        if($unit->forceId === \Wargame\Additional\EastWest\EastWest::GERMAN_FORCE){
+            return $this->findSimilarGerman($unit);
+        }
         $b = Battle::getBattle();
         /* @var mapData $mapData */
         $mapData = $b->mapData;
